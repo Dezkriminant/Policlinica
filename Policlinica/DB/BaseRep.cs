bc7637081c8c6fd57118b0392b163e1afba6f519
Artemij <artemijsnigirev40@gmail.com>
2026-05-04 23:26:41 +1000

    Общага

---
+++ Policlinica/DB/BaseRep.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using MySqlConnector;
+
+namespace Policlinica.DB;
+
+public abstract class BaseRep: IDisposable
+{
+    protected MySqlConnection connection;
+
+    public BaseRep(IOptions<DatabaseConnection> dataBaseConnection)
+    {
+        connection = new MySqlConnection(
+            dataBaseConnection.Value.ConnectionString);
+    }
+
+    public bool OpenConnection()
+    {
+        try
+        {
+            connection.Open();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+       
+    }
+
+    public bool CloseConnection()
+    {
+        try
+        {
+            connection.Close();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+        
+    }
+    
+    public void Dispose()
+    {
+        connection.Dispose();
+    }
+}
